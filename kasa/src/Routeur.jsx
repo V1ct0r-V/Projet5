81191e20ddf2be6ede5f9b452727051cfaeb1eb6
victo <victor.vagne@gmail.com>
2024-09-17 16:52:47 +0200

    MAJ du script du routeur pour compatibilité avec React 18

--- kasa/src/Routeur.jsx
+++ kasa/src/Routeur.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import Propos from "./pages/Propos.jsx";
 import FicheLogement from "./pages/FicheLogement.jsx";
 import Error from "./pages/Erreur404.jsx";
 
 function Routeur() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/a-propos" element={<Propos />} />
-          <Route path="/fiche-logement/:id" element={<FicheLogement />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-    </React.StrictMode>,
-    document.getElementById("root")
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/a-propos" element={<Propos />} />
+        <Route path="/fiche-logement/:id" element={<FicheLogement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
