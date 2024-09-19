d68c355f97bcb826b8340d09a2b806d1438c8642
victo <victor.vagne@gmail.com>
2024-09-19 21:47:58 +0200

    Correction bug de chargement des collapses

--- kasa/src/components/Collapse/Collapse.jsx
+++ kasa/src/components/Collapse/Collapse.jsx
@@ -3,26 +3,38 @@ import { useState } from "react";
 
 export default function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [firstTime, setFirstTime] = useState(true);
 
   return (
     <div className="collapse__div">
-      <div className="collapse__header" onClick={() => setIsOpen(!isOpen)}>
+      <div
+        className="collapse__header"
+        onClick={() => {
+          setIsOpen(!isOpen);
+          setFirstTime(false);
+        }}
+      >
         <p className="collapse__title">{title}</p>
         <span className="collapse__button">
-          {isOpen ? (
-            <i className="fa-solid fa-chevron-up" id="ouvrir"></i>
-          ) : (
-            <i className="fa-solid fa-chevron-up" id="fermer"></i>
-          )}
+          <i
+            className="fa-solid fa-chevron-up"
+            id={
+              firstTime === false ? (isOpen ? "ouvrir" : "fermer") : undefined
+            }
+          ></i>
         </span>
       </div>
-      <div className={`collapse__content ${isOpen ? "extended" : "collapsed"}`}>
-        {Array.isArray(content) ? (
-          content.map((item, index) => <p key={index}>{item}</p>)
-        ) : (
-          <p>{content}</p>
-        )}
-      </div>
+      {firstTime === false && (
+        <div
+          className={`collapse__content ${isOpen ? "extended" : "collapsed"}`}
+        >
+          {Array.isArray(content) ? (
+            content.map((item, index) => <p key={index}>{item}</p>)
+          ) : (
+            <p>{content}</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
